700a9ad55666824694a155989e7e6ef7aba261fc
Carlton Joseph <carlton.joseph@gmail.com>
2023-05-11 20:01:10 -0700

    add standalone endpoint

---
+++ src/routes/rng/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	let randomNumber = 0;
+
+	async function getRandomNumber() {
+		const response = await fetch(`${$page.url.pathname}/random-number`);
+		randomNumber = await response.json();
+	}
+</script>
+
+<h1>Random Number</h1>
+<h2>{randomNumber.toFixed(1)}</h2>
+
+<button on:click={getRandomNumber}>Get New Random Number</button>
